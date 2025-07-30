a21594dae0d0bf06818962d73a0fe135bf70a005
bernie-cm <bernie.camejo@icloud.com>
2025-07-30 13:26:15 +1000

    Updated etl.py with sampling call

--- etl.py
+++ etl.py
@@ -6,3 +6,10 @@ from datetime import datetime
 url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
 df = pd.read_parquet(url)
 print(f"Loaded {len(df)} records")
+
+# Take a sample of 10K records for testing
+df_sample = df.sample(n=10000, random_state=42)
+
+# Do some basic EDA
+print(df_sample.info())
+print(df_sample.head())
